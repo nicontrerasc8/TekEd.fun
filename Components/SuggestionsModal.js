640d286cf4b81994097adcc8e06158c138d4bbe4
nicontrerasc8 <nicontrerasc8@gmail.com>
2022-01-03 15:50:06 -0500

    xd

--- Components/SuggestionsModal.js
+++ Components/SuggestionsModal.js
@@ -31,7 +31,12 @@ const SuggestionsModal = ({handleClose, visible}) => {
     const [Loading, setLoading] = useState(false)
 
     const sendEmail = (e) => {
-        e.preventDefault();
+        console.log("hecho")
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000);
+        /* e.preventDefault();
         setLoading(true)
         emailjs.sendForm('service_f4cztw6', 'template_dpvw70p', form.current, 'user_kYaUkak2JxRBvs81TCFCh')
           .then((result) => {
@@ -53,32 +58,34 @@ const SuggestionsModal = ({handleClose, visible}) => {
               console.log(error.text);
               toast.error("Ocurrió un error, inténtalo de nuevo")
               setLoading(false)
-          });
+          }); */
       };
 
    
 
-    return <BackDrop onClick={handleClose} isOn={visible}>
-        {Loading && <LoadingContainer/>}
-        <motion.form 
-            className='suggestions-container' 
-            onClick={(e) => e.stopPropagation()}
-            variants={DropInFromLeft}
-            ref={form}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            onSubmit={sendEmail}
-            >
-            <h3>¡Llena el formulario para contactarnos con tu cole!</h3>
-            <input placeholder='Nombre del colegio' name='colegio' autoComplete='off'/>
-            <input placeholder='Provincia' name='provincia' autoComplete='off'/>
-            <input placeholder='Teléfono del colegio' name='celular' autoComplete='off'/>
-            <button className='btn-tertiary' type='submit'>
-                Enviar
-            </button>
-        </motion.form>
-    </BackDrop>
+    return (
+        Loading ? <LoadingContainer/> : 
+        <BackDrop onClick={handleClose} isOn={visible}>
+            <motion.form 
+                className='suggestions-container' 
+                onClick={(e) => e.stopPropagation()}
+                variants={DropInFromLeft}
+                ref={form}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                onSubmit={sendEmail}
+                >
+                <h3>¡Llena el formulario para contactarnos con tu cole!</h3>
+                <input placeholder='Nombre del colegio' name='colegio' autoComplete='off'/>
+                <input placeholder='Provincia' name='provincia' autoComplete='off'/>
+                <input placeholder='Teléfono del colegio' name='celular' autoComplete='off'/>
+                <button className='btn-tertiary' type='submit'>
+                    Enviar
+                </button>
+            </motion.form>
+        </BackDrop>
+    )
 }
 
 export default SuggestionsModal
